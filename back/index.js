865ae313df4888ab4e81c2ebd16219e6f5ce2935
NoyannOzmen <noyann.ozmen@oclock.school>
2025-04-08 15:24:05 +0200

    Improved CORS policy

--- back/index.js
+++ back/index.js
@@ -10,14 +10,7 @@ import { errorHandler, notFound } from './middlewares/errorHandlers.js';
 
 app.use(
     cors({
-      origin: [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:4173',
-        'http://127.0.0.1:4173'
-      ]
+      origin: process.env.ALLOWED_DOMAINS.split(" ")
     })
   );
 
